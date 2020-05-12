6bb1236a84e969940dc36625e492a0b72269ece7
igorpereginets <igorpereginets@gmail.com>
2020-05-12 17:15:06 +0300

    User checker added.

---
+++ src/Security/UserActiveChecker.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserActiveChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isActive()) {
+            throw new DisabledException();
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+    }
+}
